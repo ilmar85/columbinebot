e016eb4cf9b89f9a7b7fab41645db0c60d272553
ilmar85 <ilmargarifullin@gmail.com>
2019-11-26 14:37:55 +0300

    some fixes

--- src/main/java/ru/kazan/itis/columbine/labs/Bot.java
+++ src/main/java/ru/kazan/itis/columbine/labs/Bot.java
@@ -1,3 +1,5 @@
+package ru.kazan.itis.columbine.labs;
+
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -9,13 +11,19 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Bot extends TelegramLongPollingBot  {
+    private List<Long> users=new ArrayList<>();
+    final String hello = "Вас приветствует бот ColumbineLabs!";
 
     public Bot(DefaultBotOptions options) {
         super(options);
     }
 
     public static void main(String[] args) {
+//        new Properties(Bot.class.getResourceAsStream(""));
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
 
@@ -24,8 +32,31 @@ class Bot extends TelegramLongPollingBot  {
         instance.setProxyPort(9050);
         instance.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
         try {
-            telegramBotsApi.registerBot(new Bot(instance));
+            Bot bot = new Bot(instance);
+            telegramBotsApi.registerBot(bot);
+            //on action {
+            //sendToAllMessage("здесь должно быть сообщение");
+            //}
         } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Отправляет всем посетителям сообщение
+     * @param message
+     */
+    public void sendToAllMessage(String message){
+        this.users.forEach(user->{
+            sendMessage(message, user);
+        });
+    }
+
+    private void sendMessage(String message, Long user) {
+        SendMessage sendMessage = new SendMessage(user, message);
+        try {
+            this.execute(sendMessage);
+        } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
@@ -35,7 +66,8 @@ class Bot extends TelegramLongPollingBot  {
         String text = message.getText();
         if (text.contains("/start")){
             SendMessage sendMessage = new SendMessage(message.getChatId(), text);
-           sendMessage.setText("Hello Man!");
+            sendMessage.setText(hello);
+            users.add(update.getMessage().getChatId());
             try {
                 execute(sendMessage);
             } catch (TelegramApiException e) {
@@ -49,8 +81,8 @@ class Bot extends TelegramLongPollingBot  {
         return "";
     }
 
-    public String getBotToken() {
-        return "";
+        public String getBotToken() {
+            return "";
     }
 
 
